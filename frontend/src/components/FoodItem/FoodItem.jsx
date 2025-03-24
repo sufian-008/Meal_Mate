8b74badcd518142fc3919ad7122e0fa49d2b97ee
sufian008 <m.a.sufian008@gmail.com>
2025-03-25 00:09:56 +0600

    Added cart in card

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 const FoodItem = ({id,name,price,description,image}) => {
+   
+    const {cartItems, addToCart, removeFromCart} =useContext(StoreContext);
+
   return (
     <div className='food-item'>
         <div className='food-item-img-container'>
            <img className='food-item-image' src={image}/>
-
+            {
+              !cartItems[id]
+              ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white}/>
+              : <div className='food-item-counter'>
+                   <img onClick={()=>removeFromCart(id)}  src={assets.remove_icon_red}/>
+                    <p>{cartItems[id] || 0}</p>
+                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} />
+                </div>
+            }
         </div>
         <div className='food-item-info'>
            <div className='food-item-name-rating'>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,23 +1,45 @@
-import  { createContext} from "react"
-import  { food_list } from "../assets/assets"
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
+const StoreContextProvider = ({ children }) => {
+    const [cartItems, setCartItems] = useState({});
 
-const StoreContextProvider = (props)=>{
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({
+            ...prev,
+            [itemId]: (prev[itemId] || 0) + 1, // If not in cart, set to 1
+        }));
+    };
 
-
-
-    const contexValue = {
-          food_list
-    }
-
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => {
+            if (!prev[itemId]) return prev; // If item is not in cart, do nothing
+            const updatedCart = { ...prev };
+            updatedCart[itemId] -= 1;
+            if (updatedCart[itemId] <= 0) delete updatedCart[itemId]; // Remove item if count is 0
+            return updatedCart;
+        });
+    };
       
-        return (
-                <StoreContext.Provider value ={contexValue}>
-                     {props.children}
-                </StoreContext.Provider>
-        )
+    useEffect(()=>{
+        console.log(cartItems);
+    }, [cartItems])
     
-}
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
+    };
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {children}
+        </StoreContext.Provider>
+    );
+};
+
 export default StoreContextProvider;
