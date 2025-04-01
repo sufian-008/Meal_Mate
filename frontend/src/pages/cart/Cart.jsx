e7407cee143c666f467865e24c107bb61f027e93
sufian008 <m.a.sufian008@gmail.com>
2025-04-01 16:01:44 +0600

    Add Cart

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { StoreContext } from '../../context/StoreContext';
-
+import './cart.css'
 const Cart = () => {
     const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
@@ -21,19 +21,56 @@ const Cart = () => {
             {food_list && food_list.map((item, index) => {
                 if (cartItems[item._id] > 0) {
                     return (
-                        <div key={index} className="cart-items-title cart-items-item">
+                        <div>
+                           <div  className="cart-items-title cart-items-item">
                             <img src={item.image} alt={item.name} />
                             <p>{item.name}</p>
                             <p>${item.price}</p>
                             <p>{cartItems[item._id]}</p>
                             <p>${item.price * cartItems[item._id]}</p>
-                            <p onClick={() => removeFromCart(item._id)} style={{ cursor: 'pointer', color: 'red' }}>X</p>
+                            <p onClick={()=>removeFromCart(item._id)} className='cross'>X</p>
+                          </div>
+                          <hr/>
                         </div>
+                        
                     );
                 }
                
             })}
+            <div className="cart-bottom">
+                 <div className="cart-total">
+                         <h2>Cart Totals</h2>
+                         <div>
+                             <div className="cart-total-details">
+                                 <p>Subtotal</p>
+                                 <p>{0}</p>
+                             </div>
+                             <hr />
+                             <div className="cart-total-details">
+                                  <p>Delivery Fee</p>
+                                  <p>{2}</p>
+                             </div>
+                              <hr />
+                             <div className="cart-total-details">
+                                 <b>Total</b>
+                                 <b>{0}</b>
+                             </div>     
+                              <button>PROCEED TO CHECKOUT</button>                  
+                                </div>
+                                <div className="cart-promocode">
+                                    <p>If you have a promo code, Enter it here</p>
+                                    <div className='cart-promocode-input'>
+                                         <input type="text"  placeholder='promo code'/>
+                                         <button>Submit</button>
+                                    </div>
+                                </div>
+                 </div>
+               
+            </div>
+
         </div>
+        
+         
     );
 };
 
