8c04e4c2f3c9502a81cedd8ef1dd760fb69ca705
sufian008 <m.a.sufian008@gmail.com>
2025-04-25 18:37:29 +0600

    Order verify

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,12 @@
-// import express from "express"
-// import authMiddleware from "../middleware/auth.js"
-// import {placeOrder} from "../controllers/orderController.js"
+import express from "express"
+import authMiddleware from "../middleware/auth.js"
+import {placeOrder, verifyOrder} from "../controllers/orderController.js"
 
-// const orderRouter = express.Router();
+const orderRouter = express.Router();
 
-// orderRouter.post("/place", authMiddleware,placeOrder);
+orderRouter.post("/place", authMiddleware,placeOrder);
+
+orderRouter.post("/verify", verifyOrder)
 
 
-// export default orderRouter;
+export default orderRouter;
