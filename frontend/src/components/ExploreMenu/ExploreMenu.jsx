13f101d94443a1b4c22d65ac203b83c063cd7be6
sufian008 <m.a.sufian008@gmail.com>
2025-05-06 22:55:39 +0600

    Explore Menu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our Menu </h1>
-      <p className='explore-menu-text' > Choose from a diverse menu featuring a delectable array a </p>
+      <p className='explore-menu-text' > Choose from a diverse menu featuring a delectable food. </p>
       <div className='explore-menu-list'>
         {
              menu_list.map((item, index)=>{
