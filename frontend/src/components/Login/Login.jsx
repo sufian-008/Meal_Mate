8a795f85e3fa649aef8ee07af65803d9b5e67769
MD Abu Sufian <123016005+sufian-008@users.noreply.github.com>
2025-05-18 11:23:26 +0600

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -7,7 +7,7 @@ const Login = ({setShowLogin}) => {
 
    const {url, setToken} = useContext(StoreContext)
    
-      const [currState, setCurrState] = useState("Sign Up");
+      const [currState, setCurrState] = useState("Login");
       const [data,setData] = useState({
         name:"",
         email:"",
