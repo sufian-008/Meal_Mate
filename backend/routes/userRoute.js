fbca6c357d8619dfdcb5d3877beccbcf225326f0
MD Abu Sufian <123016005+sufian-008@users.noreply.github.com>
2025-09-04 09:46:10 +0600

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,10 @@
-import express from "express"
-import { loginUser, registerUser } from "../controllers/userController.js"
+import express from "express";
+import { loginUser, registerUser, getUserInfo } from "../controllers/userController.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.post("/register",registerUser)
-userRouter.post("/login",loginUser)
-
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/userinfo", getUserInfo);
 
 export default userRouter;
